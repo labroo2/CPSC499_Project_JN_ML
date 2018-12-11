987eb2da4d9eb461240d2636bd6f08c97a1eadde
Njuguna <njuguna2@illinois.edu>
2018-12-11 12:19:21 -0600

    Changes to some of the functions and rd files

--- R/structure_plot.R
+++ R/structure_plot.R
@@ -1,22 +1,23 @@
+#define the method to Visualize individual allele ancestries
 structure_plot <- function(object, ...){
   UseMethod("structure_plot", object)
 }
 structure_plot.destruct <- function(object, ...){
-  require(viridis)
-  require(ggplot2)
-  require(reshape)
-  ancestries <- as.matrix(names=rownames(object[[5]]),
-                          object[[5]][,-1])
+  #get the individual ancestry frequencies from the destruct input object
+  ancestries <- as.matrix(names=rownames(object[["individual_ancestry_frequencies"]]),
+                          object[["individual_ancestry_frequencies"]][,-1])
+  #reformat the data for a barplot
   ancestries2 <- melt(ancestries)
-  str_plot <- ggplot(data = ancestries2,  aes(x = X1, y=value, fill=X2))+
-    geom_bar(stat = "identity")+
-    scale_fill_viridis(~ X2,discrete = TRUE, option = "plasma")+
-    theme(axis.text.x=element_blank(),
-          axis.ticks.x=element_blank(),
-          axis.text.y=element_blank(),
-          axis.ticks.y=element_blank(),
-          axis.title.y=element_blank(),
-          axis.title.x=element_blank())
+  #make a barplot using ggplot2 geom bar function
+  str_plot <- ggplot(data = ancestries2,  aes_string(x = "X1", y = "value", fill = "X2")) +
+    geom_bar(stat = "identity") +
+    scale_fill_viridis(~ X2, discrete = TRUE, option = "plasma") +
+    theme(axis.text.x = element_blank(),#remove x an y axis names 
+          axis.ticks.x = element_blank(),
+          axis.text.y = element_blank(),
+          axis.ticks.y = element_blank(),
+          axis.title.y = element_blank(),
+          axis.title.x = element_blank())
   return(str_plot)
 }
 
